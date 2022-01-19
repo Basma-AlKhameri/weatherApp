6824f9788ea2296be892dc22c891d120be430a99
Basma-AlKhameri <basma21970@gmail.com>
2022-01-19 11:55:42 +0300

    fetch data and update the interface

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.weatherapp.Model.Weather
 import com.example.weatherapp.databinding.ActivityMainBinding
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     val apiKey = "11e7f34bb18820657a280840aaf0de9f"
+    val gson= Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.searchButton.setOnClickListener {
             getWeather()
+            binding.searchInput.text.clear()
         }
     }
 
@@ -31,12 +36,22 @@ class MainActivity : AppCompatActivity() {
                 val queue= Volley.newRequestQueue(this)
                 val stringRequest=StringRequest(
                     Request.Method.POST, fullURL, { response ->
-                           println(response.toString())
-                    }, {
+                        var result= gson.fromJson(response, Weather::class.java)
+                        binding.temperature.text= result.main.temp.toInt().toString()+ " C"
+                        binding.status.text= result.weather[0].main
+                        binding.textView.text= result.name+ ", "+ result.sys.country
+                        binding.maxANDmain.text= "Max "+ result.main.temp_max.toInt() + " c | Min " + result.main.temp_min.toInt() +" c"
+
+
+                    },
+                    {
+                        Toast.makeText(this, "something went wrong!",Toast.LENGTH_LONG ).show()
                         println(it.message)
                         }
                 )
                 queue.add(stringRequest)
+            }else{
+                Toast.makeText(this, "Type something", Toast.LENGTH_LONG).show()
             }
 
         }catch (err:Exception){
